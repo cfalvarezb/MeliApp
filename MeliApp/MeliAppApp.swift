6ecb59fba0fc3f1b62044c586040f55519a7f8cf
Cristian Alvarez <cristianfalvarezb@gmail.com>
2025-03-14 20:44:57 -0500

    Initial Commit

---
+++ MeliApp/MeliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliAppApp.swift
+//  MeliApp
+//
+//  Created by mac_user  on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MeliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
